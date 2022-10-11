77f832c6dbe2ff04729ffc832eafaa4b707182c1
chrisfrdly <105024168+sebasfrdly@users.noreply.github.com>
2022-10-11 07:56:46 -0400

    Points and shit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highScoreText;
 
+    int score = 0;
+    int highScore = 0;
+
+    //Start is called at first frame
+    private void Start()
+    {
+        //putting text on the screen
+        scoreText.text = score.ToString() + "POINTS";
+        highScoreText.text = "HIGHSCORE" + highScore.ToString();
+    }
+
+    //update is called every frame
+    private void Update()
+    {
+        
+    }
 
 }
